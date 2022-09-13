2df8d18a57f6df1b4b3c2e050d9e2b8298914165
Artem <105820832+Artem1207@users.noreply.github.com>
2022-09-13 12:00:39 +0300

    123

---
+++ werstka/js/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('slider-main-block', {
+  
+   loop: true,
+
+   navigation: {
+     nextEl: '.slider-main-block__arrow',
+     prevEl: '.slider-main-block__arrow'
+   },
+ 
+   
+   
+ });
